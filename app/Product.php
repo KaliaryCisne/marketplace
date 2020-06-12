5f2e649c1cc62fb94462bf80a45cb28439d05d16
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-06-11 22:41:24 -0300

    model category

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    /**
+     * Retorna a loja que possui esse produto
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    /**
+     * Retorna o usuário ao qual pertence a loja
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    /**
+     * Retorna os produtos da loja
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
