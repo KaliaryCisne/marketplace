c7b4526a58caa6bf8a1f29d87d4458eb51a58d5d
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-07-11 21:41:48 -0300

    authentication

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,18 +13,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-//        \DB::table('users')->insert(
-//            [
-//                'name' => 'Administrator',
-//                'email' => 'admin@admin.com',
-//                'email_verified_at' => now(),
-//                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-//                'remember_token' => 'vndsfkljvbfdjbn',
-//            ]
-//        );
+        \DB::table('users')->insert(
+            [
+                'name' => 'Administrator',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => 'vndsfkljvbfdjbn',
+            ]
+        );
 
-        factory(User::class, 40)->create()->each(function ($user) {
-            $user->store()->save(factory(Store::class)->make());
-        });
+//        factory(User::class, 40)->create()->each(function ($user) {
+//            $user->store()->save(factory(Store::class)->make());
+//        });
     }
 }
