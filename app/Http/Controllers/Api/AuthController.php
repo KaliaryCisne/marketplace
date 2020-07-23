da9a328df64043a1c6555b3a328a41722c77adb4
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-07-23 12:56:24 -0300

    implementing logout with jwt

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -24,6 +24,19 @@ class AuthController extends Controller
     }
 
     /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth("api")->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+
+    /**
      * Get the token array structure.
      *
      * @param  string $token
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'user.has.store' => UserHasStoreMiddleware::class
+        'user.has.store' => UserHasStoreMiddleware::class,
+        'apiJwt' => \App\Http\Middleware\apiProtectedRoute::class,
     ];
 
     /**
