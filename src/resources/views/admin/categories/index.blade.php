ccfc29ef6e1987922bf3bef0d0e68e79966ec4be
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2020-08-11 16:45:40 -0300

    layout buttons

--- src/resources/views/admin/categories/index.blade.php
+++ src/resources/views/admin/categories/index.blade.php
@@ -5,7 +5,7 @@
 
     <div class="row mt-5 mb-3">
         <div class="col">
-            <a href="{{route('admin.categories.create')}}" class="btn btn-md btn-success float-right"><i class="fa fa-plus" aria-hidden="true"></i></a>
+            <a href="{{route('admin.categories.create')}}" class="btn btn-create float-right"><i class="fa fa-plus" aria-hidden="true"></i></a>
         </div>
     </div>
 
@@ -21,13 +21,13 @@
             <tr>
                 <td class="td-no-border">{{$category->name}}</td>
                 <td class="td-no-border" width="15%">
-                    <div class="row">
-                        <a href="{{route('admin.categories.edit', ['category' => $category->id])}}" class="btn btn-md btn-primary mr-2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                    <div class="row btn-options">
+                        <a href="{{route('admin.categories.edit', ['category' => $category->id])}}" class="btn btn-edit mr-2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
 
                         <form action="{{route('admin.categories.destroy', ['category' => $category->id])}}" method="post">
                             @csrf
                             @method("DELETE")
-                            <button type="submit" class="btn btn-md btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+                            <button type="submit" class="btn btn-delete"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
                         </form>
                     </div>
                 </td>
